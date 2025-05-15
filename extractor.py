241a15d19a2a6aba4c8625b685df459290c9914c
NAILLAHHHH <n.ineza1@alustudent.com>
2025-05-15 13:01:21 +0200

    Updated extractor.py

--- extractor.py
+++ extractor.py
@@ -1,17 +1,22 @@
 import re
 
 def extract_emails(text):
-    return re.findall(r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b', text)
+    pattern = r'\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b'
+    return re.findall(pattern, text)
 
 def extract_urls(text):
-    return re.findall(r'https?://[^\s<>"]+|www\.[^\s<>"]+', text)
+    pattern = r'\bhttps?://[^\s<>"]+|www\.[^\s<>"]+\b'
+    return re.findall(pattern, text)
 
 def extract_phone_numbers(text):
-    return re.findall(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}', text)
+    pattern = r'\b(?:\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4})\b'
+    return re.findall(pattern, text)
 
 def extract_credit_cards(text):
-    return re.findall(r'\b(?:\d{4}[-\s]?){3}\d{4}\b', text)
+    pattern = r'\b(?:\d{4}[-\s]){3}\d{4}\b'
+    return re.findall(pattern, text)
 
 def extract_times(text):
-    return re.findall(r'\b(?:[01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?\b', text)
-
+    pattern = r'\b((1[0-2]|0?[1-9]):[0-5]\d\s?[APap][Mm]|(2[0-3]|[01]?\d):[0-5]\d)\b'
+    return re.findall(pattern, text)
+    
